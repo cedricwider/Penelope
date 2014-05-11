31bf56a4c1df907a5e0d3b273502d43750606b6d
Cedric Wider <wider.cedric@gmail.com>
2014-05-11 14:24:07 +0200

    Heroku MongoDB changes

--- models/init.rb
+++ models/init.rb
@@ -1,5 +1,19 @@
 require 'mongo'
 
-DB = Mongo::Connection.new.db("youser", :pool_size => 5, :timeout => 5)
+def get_connection
+  return @db_connection if @db_connection
+
+  connection_settings = URI.parse(ENV['MONGOHQ_URL'])
+  if connection_settings
+    db_name = connection_settings.path.gsub(/^\//, '')
+    @db_connection = Mongo::Connection.new(connection_settings.host, connection_settings.port).db(db_name)
+    @db_connection.authenticate(connection_settings.user, connection_settings.password) unless (connection_settings.user.nil? || connection_settings.password.nil?)
+  else
+    @db_connection = Mongo::Connection.new.db("youser", :pool_size => 5, :timeout => 5)
+  end
+  @db_connection
+end
+
+DB = get_connection
 
 require_relative 'user_data'

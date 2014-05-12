66e96a3d96ae0c4dd14292020d10a62835d31a60
Cedric Wider <wider.cedric@gmail.com>
2014-05-12 19:44:59 +0200

    add logout functionality

--- routes/login_controller.rb
+++ routes/login_controller.rb
@@ -1,10 +1,13 @@
 class Penelope < Sinatra::Application
+
   post '/login' do
-    user_doc = DB.collection('user').find_one({:email=>params['email']})
-    if user_doc.nil? || DB.collection('logindata').find_one(:user_id=>user_doc['_id'], :password => params['password']).nil?
-      return haml :index, :attr_wrapper => '"', :locals => {:title => 'Welcome', :user => false}
+    user_doc = find_user(params['email'], params['password'])
+    if user_doc.nil?
+      redirect to('/')
     end
+
     @user = User.new
+    @user._id = user_doc['_id']
     @user.email = user_doc['email']
     @user.firstname =user_doc['firstname']
     @user.lastname =user_doc['lastname']
@@ -17,21 +20,37 @@ class Penelope < Sinatra::Application
   end
 
   post '/signup' do
-    user = User.new
-    user.email = params['email']
-    user.firstname = params['firstname']
-    user.lastname = params['lastname']
+    @user = User.new
+    @user.email = params['email']
+    @user.firstname = params['firstname']
+    @user.lastname = params['lastname']
     unless DB.collection('user').find_one({:email => user.email}).nil?
       return haml :signup, :attr_wrapper => '"', :locals => {errors: ['existing_email']}
     end
 
-    user_id = DB.collection('user').insert({:email=>user.email, :firstname=>user.firstname, :lastname=>user.lastname})
-    user._id = user_id
+    user_id = DB.collection('user').insert({:email=>@user.email, :firstname=>@user.firstname, :lastname=>@user.lastname})
+    @user._id = user_id
     login_request = LoginData.new
     login_request.password = params['password']
     login_request.user_id = user_id
     DB.collection('logindata').insert({:user_id=>user_id, :password=>login_request.password})
-    session['user'] =user
+    session['user'] =@user
     haml :home, :attr_wrapper => '"'
+  end
+
+  get '/logout' do
+    @user = nil
+    session.clear
+    redirect to('/')
+  end
+
+  def find_user(username, password)
+    user_doc = DB.collection('user').find_one({:email=>params['email']})
+    login_data = nil
+    unless user_doc.nil?
+      login_data = DB.collection('logindata').find_one(:user_id=>user_doc['_id'], :password => params['password'])
+    end
+
+    login_data.nil? ? nil : user_doc
   end
 end
